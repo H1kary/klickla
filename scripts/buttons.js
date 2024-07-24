443fa1dcfd73cb4474352e3f6471f24eb3848231
H1kary <79506816+H1kary@users.noreply.github.com>
2024-07-24 21:31:05 +0300

    Add files via upload

---
+++ scripts/buttons.js
@@ -0,0 +1,11 @@
+document.querySelector('.download-modpack').addEventListener('click', function() {
+    window.open('https://drive.google.com/file/d/1B29cFHwAcgoE4jVY_zXfGuY2X33CY6GA/view?usp=sharing', '_blank');
+});
+
+document.querySelector('.step-rules').addEventListener('click', function() {
+    window.open('rules.html');
+});
+
+document.querySelector('.step-discord').addEventListener('click', function() {
+    window.open('https://discord.gg/MpkMZzGceD');
+});
